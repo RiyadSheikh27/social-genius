46f3597b3f148c184d518676be2141d9f8c1b196
RiyadSheikh27 <riyad.cse27@gmail.com>
2025-12-10 17:30:06 +0600

    latest code with rds integration

--- main/settings.py
+++ main/settings.py
@@ -103,10 +103,20 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'socialgenius123',
+        'HOST': 'django-db.cq18gg0wsbu5.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
